c28f0c40862495050172506eefc922246b51645a
digitalmaith <digitalmaith@gmail.com>
2026-08-11 14:47:55 +0000

    chore: init gaynaako-frontend (Vite + React + TS + Tailwind)

---
+++ src/shared/services/api.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      // TODO: logique refresh token / redirection login
+    }
+    return Promise.reject(err);
+  }
+);
